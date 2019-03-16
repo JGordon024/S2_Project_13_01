eb386a467a16481dfd7a18d8a490b6b2af1b8c16
Unknown <jgordon024@west-mec.org>
2019-03-15 18:24:31 -0700

    More Functions+Comments
    I created another function that will turn off the stars when they're not being hovered above. Also fixed my code so that the stars will show. Finally, I added my comments

--- bw_review.js
+++ bw_review.js
@@ -33,28 +33,50 @@
       within textStr
 
 */
+
 window.onload = init;
 //Creates a function to identify the type of control that performs an action
-
 function init() {
       var stars = document.querySelectorAll("span#stars img");
-      for (var i = 0; i <= stars.length; i++) {
-            stars[i].style.cursor = "pointer"
-            stars[i].addEventListener("mouseenter", lightStars());
-
+      // Creates a for loop that will allow for the mouse to become a pointer when hovered above the 5 stars
+      for (var i = 0; i < stars.length; i++) {
+            stars[i].style.cursor = "pointer";
+            stars[i].addEventListener("mouseenter", lightStars);
       }
-      document.getElementById("commentField").addEventListener("keyup", Count());
+      document.getElementById("comment").addEventListener("keyup", updateCount);
 }
 
 function lightStars(e) {
-      var starNumber = event.target.alt;
+      // Sets the variables 
+      var starNumber = e.target.alt;
+      // This variable is applying the star images CSS 
       var stars = document.querySelectorAll("span#stars img");
-      for (var i = 0; i < starNumber.length; i++) {
-            stars[i].src = "url(bw_star2.png)";
+      // Creates a for loop that will allow the user to choose the amount of stars
+      for (var i = 0; i < starNumber; i++) {
+            stars[i].src = "bw_star2.png";
+
+      }
+      // Will unselect the stars, replacing it with an empty star when not hovered above
+      for (var i = starNumber; i < 5; i++) {
+            stars[i].src = "bw_star.png";
+
+      }
+      // Will use the text box area next to the stars state how many stars are selected
+      document.getElementById("rating").value = starNumber + " star(s)";
+      // Targets the event for when the mouse is off of the stars, which will launch the turnOffStars function
+      e.target.addEventListener("mouseleave", turnOffStars);
+      e.target.addEventListener("mouseleave", function () {
+            document.removeEventListener("mouseleave", turnOffStars);
+      });
+}
+//Creates a function that will turn off the stars when the mouse is not hovering above them
+function turnOffStars(e) {
+      var stars = document.querySelectorAll("span#stars img");
+      for (var i = 0; i < stars.length; i++) {
+            stars[i].src = "bw_star.png";
+            document.getElementById("rating").value = "";
       }
 }
-
-
 
 
 
